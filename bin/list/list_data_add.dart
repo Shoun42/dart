f35e76e600b909425b2ebc247c3deb11c69f4ec1
Shoun42 <shounbabu31@gmail.com>
2025-02-21 10:46:35 +0600

    20-02-2025

---
+++ bin/list/list_data_add.dart
@@ -0,0 +1,12 @@
+main (){
+  var num=[1,2,3,4,5,6,7];
+  print(num);
+  num.add(8); // sudu একটা iteam এড হবে
+  print(num);
+  num.addAll([9,10,11,12,13,14]); // oneak iteam এড হবে
+  print(num);
+  num.insert(9, 100); // sudu একটা iteam পছন্দ এর জায়গা এড হবে
+  print(num);
+  num.insertAll(8,[9, 100, 200,300,400,]); // issa moto iteam পছন্দ এর জায়গা এড jay
+  print(num);
+}
---
+++ bin/list/list_update.dart
@@ -0,0 +1,9 @@
+void main (){
+  var num= [1,2,3,4,5,6];
+  num[2]=100;
+  num[3]=200;
+  num[4]=300;
+  num[5]=300;
+  print(num);
+
+}

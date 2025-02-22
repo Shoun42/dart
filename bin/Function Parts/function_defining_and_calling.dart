04b272575fb95e836ad933156fcd2464becf29f5
Shoun42 <shounbabu31@gmail.com>
2025-02-22 23:03:53 +0600

    22-02-2025

---
+++ bin/Function Parts/function_defining_and_calling.dart
@@ -0,0 +1,12 @@
+addThreeNumber (){
+  var x= 1; //defind function ai line a
+  var y= 3;  //defind function ai line a
+  var z= 2;   //defind function ai line a
+
+  print(x+y+z);
+
+}
+
+main(){
+  addThreeNumber();  //Function Call korsi ai line a
+}
---
+++ bin/Function Parts/passing_paramiter.dart
@@ -0,0 +1,14 @@
+addThreeNumber(int x,int y,int z){
+  print(x+y+z);
+}
+
+
+main(){
+  addThreeNumber(10, 20, 15);
+  addThreeNumber(15, 20, 15);
+  addThreeNumber(19, 28, 15);
+  addThreeNumber(10, 20, 20);
+  addThreeNumber(10, 20, 15);
+  addThreeNumber(10, 15, 15);
+
+}

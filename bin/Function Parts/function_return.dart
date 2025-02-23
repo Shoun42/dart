bf47a48706d3be20f62f56f8360eda4a64cb91f8
Shoun42 <shounbabu31@gmail.com>
2025-02-23 20:39:19 +0600

    23-02-2025

---
+++ bin/Function Parts/function_return.dart
@@ -0,0 +1,9 @@
+addThreeNumber(int x,int y,int z){
+  return(x+y+z); //return only output return korbe = kaj somporno korbe but kono result print korbe na...
+}
+
+
+main(){
+  var result= addThreeNumber(10, 20, 15);
+    print(result);
+}

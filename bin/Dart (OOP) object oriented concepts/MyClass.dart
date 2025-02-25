0594d792345ca243af3c02edc957905dd6a175a5
Shoun42 <shounbabu31@gmail.com>
2025-02-25 16:53:54 +0600

    24-02-2025

---
+++ bin/Dart (OOP) object oriented concepts/MyClass.dart
@@ -0,0 +1,14 @@
+class MyClass{
+
+  var Sagor ='My Name is Prince';      //proparty aigula
+  var Alphabet =['A','B','C','D'];     //proparty aigula
+
+  addTwoNumber (int x,int y) {         //proparty aigula
+    print(x + y);
+  }
+  addThreeNumber(int x, int y,int z){   //proparty aigula
+    print(x+y+z);
+  }
+
+
+}
---
+++ bin/Dart (OOP) object oriented concepts/oop_2nd.dart
@@ -0,0 +1,16 @@
+import 'MyClass.dart';
+
+void main(){
+
+  var obj= new MyClass( );   //class ar object ata abok ata conostactor call kora
+
+  obj.addTwoNumber(10, 30);
+
+  obj.addThreeNumber(10, 20,30);
+
+
+  print(obj.Sagor);
+  print(obj.Alphabet[2]);
+
+
+}

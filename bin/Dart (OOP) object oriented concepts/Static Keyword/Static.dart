db1d556482acd5e26ac9b230f26767989c0bbf23
Shoun42 <shounbabu31@gmail.com>
2025-02-27 00:27:04 +0600

    25-02-2025

---
+++ bin/Dart (OOP) object oriented concepts/Static Keyword/Static.dart
@@ -0,0 +1,14 @@
+class MyClass{
+
+  static var Sagor ='My Name is Prince';      //proparty aigula
+  static var Alphabet =['A','B','C','D'];     //proparty aigula
+
+  static addTwoNumber (int x,int y) {         //proparty aigula
+    print(x + y);
+  }
+  static addThreeNumber(int x, int y,int z){   //proparty aigula
+    print(x+y+z);
+  }
+
+
+}

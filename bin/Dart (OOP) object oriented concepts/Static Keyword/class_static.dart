a9b46f923d6ae854c1d32c54e93d72ac3e965f3a
Shoun42 <shounbabu31@gmail.com>
2025-02-27 00:32:59 +0600

    26-02-2025

---
+++ bin/Dart (OOP) object oriented concepts/Static Keyword/class_static.dart
@@ -0,0 +1,16 @@
+import 'Static.dart';
+
+void main(){
+
+  var obj= new MyClass( );   //class ar object ata abok ata conostactor call kora
+
+  MyClass.addTwoNumber(10, 30);
+
+  MyClass.addThreeNumber(10, 20,30);
+
+
+  print(MyClass.Sagor);
+  print(MyClass.Alphabet[2]);
+
+
+}

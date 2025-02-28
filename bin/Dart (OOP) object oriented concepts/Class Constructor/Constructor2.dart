a2593cb4da40caa6242b00994af7f6be11f5be36
Shoun42 <shounbabu31@gmail.com>
2025-02-28 19:20:19 +0600

    28-02-2025

---
+++ bin/Dart (OOP) object oriented concepts/Class Constructor/Constructor2.dart
@@ -0,0 +1,6 @@
+import 'Constructor_Function.dart';
+
+void main (){
+  var obj = ClassName('I am a constructor paramiter'); //ai khane parentatis ar modda para miter patabo//obj make korar shate shate call hobe
+
+}

a68fa09008b064f8f98c85366708d442be0c3fd7
Shoun42 <shounbabu31@gmail.com>
2025-02-28 19:33:26 +0600

    28-02-2025

--- bin/Dart (OOP) object oriented concepts/Class Constructor/constructor2.dart
+++ bin/Dart (OOP) object oriented concepts/Class Constructor/constructor2.dart
@@ -1,6 +1,6 @@
 import 'constructor_function.dart';
 
 void main (){
-  var obj = ClassName('I am a constructor paramiter'); //ai khane parentatis ar modda para miter patabo//obj make korar shate shate call hobe
+  var obj = ClassName('I am constructor paramiter'); //ai khane parentatis ar modda para miter patabo//obj make korar shate shate call hobe
 
 }
--- bin/Dart (OOP) object oriented concepts/Class Constructor/constructor_function.dart
+++ bin/Dart (OOP) object oriented concepts/Class Constructor/constructor_function.dart
@@ -3,10 +3,10 @@ class ClassName {
  ClassName (String msg){ //para miter recive korar jonno variable nibo msg //jodi class ar vitor a oi class ar name a kono functin turiy kori take constructor function bole
 
   print(msg);
-  //print('I am a Constructor');
+     /*//print('I am a Constructor');*/
                    // constactor function mathod ke call korte hoy na auto matic call hoy
-                //return 'result';  Constactor function a kono kisu return kora jay na
+                   //return 'result';  Constactor function a kono kisu return kora jay na
 
-  }
+    }
 
  }
